33c9585618ac8fd2a45eddf53d21a83cfd276dd8
song <871609160@qq.com>
2020-02-07 18:44:02 +0800

    t

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -13,22 +13,17 @@ class UsersExport implements FromView,ShouldAutoSize
     {
         $id_card=request('id_card',null);
         $name=request('name',null);
+        $areas=request('areas',null);
         $name=urlencode(trim($name));
         $id_card=urlencode(trim($id_card));
-        if ($id_card&&$name){
-            $body=file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?idCard=".$id_card."&name=".$name);
-        }elseif ($id_card){
-
-            $body=file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?idCard=".$id_card);
-        }elseif ($name){
-            $body=file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?name=".$name);
-        }else{
-            $body=file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?idCard=&name=");
-        }
-
+        $areas=urlencode(trim($areas));
+        $body=file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?idCard=".$id_card."&name=".$name."&areas=".$areas."&currentPageNo=".request('page',1)."&pageSize=".request('pageSize',1));
         $body=json_decode($body,true);
+        $total=$body['data']['total'];
+        $body1=file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?idCard=".$id_card."&name=".$name."&areas=".$areas."&currentPageNo=".request('page',1)."&pageSize=".$total);
+        $body1=json_decode($body1,true);
         return view('admin.export', [
-            'list' => $body
+            'list' => $body1
         ]);
     }
 
