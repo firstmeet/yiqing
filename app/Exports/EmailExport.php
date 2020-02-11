bea1b522c8841047584d55e90f20ab5d5b1b132b
song <871609160@qq.com>
2020-02-11 23:42:32 +0800

    t

--- app/Exports/EmailExport.php
+++ app/Exports/EmailExport.php
@@ -19,16 +19,11 @@ class EmailExport implements FromCollection,WithHeadings,ShouldAutoSize
     */
     public function collection()
     {
-        $id_card = request('id_card', null);
-        $name = request('name', null);
-        $areas = request('areas', null);
-        $name = urlencode(trim($name));
-        $id_card = urlencode(trim($id_card));
-        $areas = urlencode(trim($areas));
-        $body = file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?idCard=" . $id_card . "&name=" . $name . "&areas=" . $areas . "&currentPageNo=" . request('page', 1) . "&pageSize=" . request('pageSize', 1));
+
+        $body = file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?pageSize=1");
         $body = json_decode($body, true);
         $total = $body['data']['total'];
-        $body1 = file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?idCard=" . $id_card . "&name=" . $name . "&areas=" . $areas . "&currentPageNo=" . request('page', 1) . "&pageSize=" . $total);
+        $body1 = file_get_contents("http://112.29.244.243:9999/yiqing-register/register/querySomth?pageSize=" . $total);
         $body1 = json_decode($body1, true);
         $data = collect($body1['data']['rows'])->keyBy("idCard");
         $time=$this->time;
