1010501ba4ce73d11f1f7da5f7740e9fc39786df
song <871609160@qq.com>
2020-02-12 11:09:53 +0800

    t

--- resources/views/admin/tongji.blade.php
+++ resources/views/admin/tongji.blade.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="/js/extendPagination.js"></script>
     <script src="/js/vue.min.js"></script>
     <link href="https://cdn.bootcss.com/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
     <script src="https://cdn.bootcss.com/moment.js/2.22.1/moment-with-locales.min.js"></script>
@@ -33,37 +32,6 @@
 <div id="loading"></div>
 <div id="app" v-cloak>
     <form role="form" class="form-inline">
-        {{--        <div class="form-group">--}}
-        {{--            <label for="name" class="col-sm-3 control-label" style="line-height: 34px">姓名</label>--}}
-        {{--            <div class="col-sm-9">--}}
-        {{--                <input type="text" class="form-control" id="name" v-model="name" placeholder="请输入姓名">--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
-        {{--        <div class="form-group">--}}
-        {{--            <label for="id_card" class="col-sm-4 control-label" style="line-height: 34px">身份证号</label>--}}
-        {{--            <div class="col-sm-8">--}}
-        {{--                <input type="text" class="form-control" id="id_card" v-model="id_card" placeholder="请输入身份证号">--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
-        {{--        <div class="form-group">--}}
-        {{--            <label for="areas" class="col-sm-4 control-label" style="line-height: 34px">来淮南入住县/区</label>--}}
-        {{--            <div class="col-sm-8">--}}
-        {{--                <select name="areas" id="areas" v-model="areas" class="form-control">--}}
-        {{--                    <option value>请选择...</option>--}}
-        {{--                    <option value="寿县">寿县</option>--}}
-        {{--                    <option value="凤台县">凤台县</option>--}}
-        {{--                    <option value="大通区">大通区</option>--}}
-        {{--                    <option value="田家庵区">田家庵区</option>--}}
-        {{--                    <option value="谢家集区">谢家集区</option>--}}
-        {{--                    <option value="八公山区">八公山区</option>--}}
-        {{--                    <option value="潘集区">潘集区</option>--}}
-        {{--                    <option value="淮南市毛集社会发展综合实验区">淮南市毛集社会发展综合实验区</option>--}}
-        {{--                    <option value="淮南经济技术开发区">淮南经济技术开发区</option>--}}
-        {{--                    <option value="淮南高新区管委会">淮南高新区管委会</option>--}}
-        {{--                    <option value="淮南新桥国际产业园">淮南新桥国际产业园</option>--}}
-        {{--                </select>--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
         <div class="form-group">
             <label for="datetime" class="col-sm-4 control-label" style="line-height: 34px">选择日期</label>
             <div class="col-sm-8">
@@ -102,84 +70,8 @@
         </tbody>
     </table>
 </div>
-{{--<div class="test">--}}
-{{--    <navigation :pages="pages" :current.sync="current" @navpage="getMan" ></navigation>--}}
-{{--    <ul class="pagination pagination-lg" >--}}
-{{--        <li>--}}
-{{--            <select name="pageSize"  class="form-control pageSize" v-model="pageSize" style="height: 46px">--}}
-{{--                <option value="10">10</option>--}}
-{{--                <option value="20" >20</option>--}}
-{{--                <option value="50" >50</option>--}}
-{{--                <option value="100">100</option>--}}
-{{--                <option value="500">500</option>--}}
-{{--            </select></li>--}}
-{{--    </ul>--}}
-{{--</div>--}}
 
 <script>
-    // var pageComponent = Vue.extend({
-    //     template: `<nav aria-label="Page navigation">
-    //     <ul class="pagination pagination-lg" style="float: left">
-    //         <li :class="{\'disabled\':curPage==1}">
-    //             <a href="javascript:;" @click="goPage(curPage==1?1:curPage-1)" aria-label="Previous">
-    //                 <span aria-hidden="true">&laquo;</span>
-    //             </a>
-    //         </li>
-    //         <li v-for="page in showPageBtn" :class="{\'active\':page==curPage}">
-    //             <a href="javascript:;" v-if="page" @click="goPage(page)">@{{page}}</a>
-    //             <a href="javascript:;" v-else>...</a>
-    //         </li>
-    //         <li :class="{\'disabled\':curPage==pages}">
-    //             <a href="javascript:;" @click="goPage(curPage==pages?pages:curPage+1)" aria-label="Next">
-    //                 <span aria-hidden="true">&raquo;</span>
-    //             </a>
-    //         </li>
-    //     </ul>
-    // </nav>`,
-    //     props: {
-    //         pages: {
-    //             type: Number,
-    //             default: 1
-    //         },
-    //         current: {
-    //             type: Number,
-    //             default: 1
-    //         }
-    //     },
-    //     data(){
-    //         return{
-    //             curPage:1
-    //         }
-    //     },
-    //     computed: {
-    //         showPageBtn() {
-    //             let pageNum = this.pages;
-    //             let index = this.curPage;
-    //             let arr = [];
-    //             if (pageNum <= 5) {
-    //                 for (let i = 1; i <= pageNum; i++) {
-    //                     arr.push(i)
-    //                 }
-    //                 return arr
-    //             }
-    //             if (index <= 2) return [1, 2, 3, 0, pageNum];
-    //             if (index >= pageNum - 1) return [1, 0, pageNum - 2, pageNum - 1, pageNum];
-    //             if (index === 3) return [1, 2, 3, 4, 0, pageNum];
-    //             if (index === pageNum - 2) return [1, 0, pageNum - 3, pageNum - 2, pageNum - 1, pageNum];
-    //             return [1, 0, index - 1, index, index + 1, 0, pageNum];
-    //         }
-    //     },
-    //     methods: {
-    //         goPage(page) {
-    //             if (page != this.curPage) {
-    //                 this.curPage = page;
-    //                 this.$emit('navpage', this.curPage);
-    //             }else{
-    //             }
-    //         }
-    //     }
-    // });
-    // Vue.component('navigation', pageComponent);
 
     var vm=new Vue({
         el:"#app",
